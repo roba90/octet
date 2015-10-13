1c7223cc6c97d7ddc96d63fcabdae77cff95062f
roba90 <ralle015@gold.ac.uk>
2015-10-13 15:46:03 +0100

    Change Background Colour

--- octet/src/examples/example_invaderers/invaderers_app.h
+++ octet/src/examples/example_invaderers/invaderers_app.h
@@ -521,7 +521,7 @@ namespace octet {
       glViewport(x, y, w, h);
 
       // clear the background to black
-      glClearColor(0, 0, 0, 1);
+      glClearColor(0, 1, 0, 1);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
       // don't allow Z buffer depth testing (closer objects are always drawn in front of far ones)
